e549c1b9410f8c8b9200cf645726adfb7a52d596
Laíse Carvalho <laisecrv@gmail.com>
2021-05-11 16:01:26 -0300

    feat: add classes for pokemon cards

--- js/main.js
+++ js/main.js
@@ -2,37 +2,37 @@ const pokeContainer = document.querySelector('.pokemons-container');
 const totalPokemon = 150;
 
 const getPokemon = async () => {
-   for (let i = 1; i <= totalPokemon; i++) {
-      await fetchPokemon(i);
-   }
+  for (let i = 1; i <= totalPokemon; i++) {
+    await fetchPokemon(i);
+  }
 };
 
 const fetchPokemon = async id => {
-   const url = 'https://pokeapi.co/api/v2/pokemon';
-   const response = await fetch(`${url}/${id}`);
-   const pokemonData = await response.json();
+  const url = 'https://pokeapi.co/api/v2/pokemon';
+  const response = await fetch(`${url}/${id}`);
+  const pokemonData = await response.json();
 
-   createPokemonCard(pokemonData);
+  createPokemonCard(pokemonData);
 };
 
-const createPokemonCard = pokemon => {
-   const pokeCard = document.createElement('div');
-   pokeCard.classList.add('pokemon-card');
+const createPokemonCard = async pokemon => {
+  const pokeCard = document.createElement('div');
 
-   const {id, types, name, sprites} = pokemon;
+  const { id, types, name, sprites } = pokemon;
+  pokeCard.classList.add('pokemon-card', `${types[0].type.name}`);
 
-   pokeCard.innerHTML = `
+  pokeCard.innerHTML = `
       <div class="pokemon-image">
          <img src=${sprites.front_default} alt=${name} />
       </div>
       <div class="pokemon-info">
          <span>#${id}</span>
-         <h3>${name}</h3>
+         <h3 id="name">${name}</h3>
          <p class="type">${types.map(typeName => typeName.type.name).join(' | ')}</p>
       </div>
    `;
 
-   pokeContainer.appendChild(pokeCard);
+  pokeContainer.appendChild(pokeCard);
 };
 
 getPokemon();
