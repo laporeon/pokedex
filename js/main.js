144130d367bc788802f58ebc7da5c14fc11cf644
Laíse Carvalho <laisecrv@gmail.com>
2021-08-16 20:53:43 -0300

    feat: import new modules

--- js/main.js
+++ js/main.js
@@ -1,38 +1,8 @@
-const pokeContainer = document.querySelector('.pokemons-container');
-const totalPokemon = 150;
+import LoadingScreen from './modules/loading-screen.js';
+import FetchPokemon from './modules/fetch-pokemons.js';
 
-const getPokemon = async () => {
-  for (let i = 1; i <= totalPokemon; i++) {
-    await fetchPokemon(i);
-  }
-};
+const loadingScreen = new LoadingScreen();
+loadingScreen.init();
 
-const fetchPokemon = async id => {
-  const url = 'https://pokeapi.co/api/v2/pokemon';
-  const response = await fetch(`${url}/${id}`);
-  const pokemonData = await response.json();
-
-  createPokemonCard(pokemonData);
-};
-
-const createPokemonCard = async pokemon => {
-  const pokeCard = document.createElement('div');
-
-  const { id, types, name, sprites } = pokemon;
-  pokeCard.classList.add('pokemon-card', `${types[0].type.name}`);
-
-  pokeCard.innerHTML = `
-      <div class="pokemon-image">
-         <img src=${sprites.front_default} alt=${name} />
-      </div>
-      <div class="pokemon-info">
-         <span>#${id}</span>
-         <h3 id="name">${name}</h3>
-         <p class="type">${types.map(typeName => typeName.type.name).join(' | ')}</p>
-      </div>
-   `;
-
-  pokeContainer.appendChild(pokeCard);
-};
-
-getPokemon();
+const fetchPokemon = new FetchPokemon();
+fetchPokemon.init();
